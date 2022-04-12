1ff5cfdced17603f517e8d7a20b5a1873c11cf84
Matt Hook <hookenz@gmail.com>
2022-04-13 07:58:37 +1200

    add Linode and Digital Ocean support

--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module github.com/kotajacob/kaaskill
 
 go 1.18
 
-require github.com/civo/civogo v0.2.74
+require (
+	github.com/civo/civogo v0.2.74
+	github.com/digitalocean/godo v1.78.0
+	github.com/linode/linodego v1.4.1
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+)
+
+require (
+	github.com/go-resty/resty/v2 v2.1.1-0.20191201195748-d7b97669fe48 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,55 +2,46 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
 
-	"github.com/civo/civogo"
+	civo "github.com/civo/civogo"
+	"github.com/digitalocean/godo"
+	linode "github.com/linode/linodego"
+
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	civoKey, ok := os.LookupEnv("CIVO_API_KEY")
-	if !ok {
-		log.Fatalln("missing required CIVO_API_KEY environment variable")
-	}
+	for {
+		selection := StringPrompt("\n(1) Civo\n(2) Linode\n(3) Digital Ocean\n(q) quit\n\nChoose provider: ")
+		if selection == "q" {
+			return
+		}
 
-	client, err := civogo.NewClient(civoKey, "")
-	if err != nil {
-		log.Fatalf("failed creating new Civo client: %v", err)
-	}
+		index, err := strconv.Atoi(selection)
+		if err != nil {
+			log.Fatalf("invalid entry %v selection must be a number", selection)
+		}
 
-	clusters, err := client.ListKubernetesClusters()
-	if err != nil {
-		log.Fatalf("failed listing all instances: %v", err)
-	}
-	if len(clusters.Items) == 0 {
-		fmt.Println("There are no clusters currently under this account")
-		os.Exit(0)
-	}
+		fmt.Println()
 
-	// Print a list of clusters and ask which ones to delete.
-	indexedClusters := make(map[int]string, len(clusters.Items))
-	for i, cluster := range clusters.Items {
-		fmt.Printf("%v: %v\n", i, cluster.Name)
-		indexedClusters[i] = cluster.ID
+		switch index {
+		case 1:
+			kaasKillCivo()
+		case 2:
+			kaasKillLinode()
+		case 3:
+			kaasKillDigitalOcean()
+		default:
+			log.Fatalf("invalid entry %v selection must be a number", selection)
+		}
 	}
-	selection := StringPrompt("Enter cluster index number to remove: ")
-
-	// Try to delete selected cluster index number.
-	index, err := strconv.Atoi(selection)
-	if err != nil {
-		log.Fatalf("invalid entry %v selection must be a number", selection)
-	}
-
-	id, ok := indexedClusters[index]
-	if !ok {
-		log.Fatalf("invalid cluster index: should be between 0-%v", len(clusters.Items)-1)
-	}
-	client.DeleteKubernetesCluster(id)
-	fmt.Println("Successfully deleted cluster", clusters.Items[index].Name)
 }
 
 // StringPrompt asks for a string value using the label
@@ -65,4 +56,143 @@ func StringPrompt(label string) string {
 		}
 	}
 	return strings.TrimSpace(s)
+}
+
+func kaasKillCivo() {
+	apikey, ok := os.LookupEnv("CIVO_API_KEY")
+	if !ok {
+		log.Fatalln("missing required CIVO_API_KEY environment variable")
+	}
+
+	client, err := civo.NewClient(apikey, "")
+	if err != nil {
+		log.Fatalf("failed creating new Civo client: %v", err)
+	}
+
+	clusters, err := client.ListKubernetesClusters()
+	if err != nil {
+		log.Fatalf("failed listing all instances: %v", err)
+	}
+	if len(clusters.Items) == 0 {
+		fmt.Println("There are no clusters currently under this account")
+		return
+	}
+
+	for {
+		// Print a list of clusters and ask which ones to delete.
+		indexedClusters := make(map[int]string, len(clusters.Items))
+		for i, cluster := range clusters.Items {
+			fmt.Printf("(%v) %v\n", i, cluster.Name)
+			indexedClusters[i] = cluster.ID
+		}
+		fmt.Printf("(b) go back\n\n")
+		selection := StringPrompt("Enter cluster index number to remove: ")
+		if selection == "b" {
+			return
+		}
+
+		// Try to delete selected cluster index number.
+		index, err := strconv.Atoi(selection)
+		if err != nil {
+			log.Fatalf("invalid entry %v selection must be a number", selection)
+		}
+
+		id, ok := indexedClusters[index]
+		if !ok {
+			log.Fatalf("invalid cluster index: should be between 0-%v", len(clusters.Items)-1)
+		}
+		client.DeleteKubernetesCluster(id)
+		fmt.Println("Successfully deleted cluster", clusters.Items[index].Name)
+	}
+}
+
+func kaasKillLinode() {
+	apikey, ok := os.LookupEnv("LINODE_API_KEY")
+	if !ok {
+		log.Fatalln("missing required LINODE_API_KEY environment variable")
+	}
+
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: apikey})
+
+	oauth2Client := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: tokenSource,
+		},
+	}
+
+	client := linode.NewClient(oauth2Client)
+
+	for {
+		clusters, err := client.ListLKEClusters(context.Background(), nil)
+		if err != nil {
+			log.Fatalf("failed listing all instances: %v", err)
+		}
+
+		if len(clusters) == 0 {
+			fmt.Println("There are no clusters currently under this account")
+			return
+		}
+
+		for i, cluster := range clusters {
+			fmt.Printf("(%v) %v\n", i+1, cluster.Label)
+		}
+		fmt.Printf("(b) go back\n\n")
+		selection := StringPrompt("Enter cluster index number to remove: ")
+		if selection == "b" {
+			return
+		}
+
+		// Try to delete selected cluster index number.
+		index, err := strconv.Atoi(selection)
+		if err != nil {
+			log.Fatalf("invalid entry %v selection must be a number", selection)
+		}
+
+		err = client.DeleteLKECluster(context.Background(), clusters[index-1].ID)
+		if err != nil {
+			log.Fatalf("failed deleting instance: %v", err)
+		}
+	}
+}
+
+func kaasKillDigitalOcean() {
+	apikey, ok := os.LookupEnv("DIGITAL_OCEAN_API_KEY")
+	if !ok {
+		log.Fatalln("missing required DIGITAL_OCEAN_API_KEY environment variable")
+	}
+
+	client := godo.NewFromToken(apikey)
+	ctx := context.TODO()
+
+	for {
+		clusters, _, err := client.Kubernetes.List(ctx, nil)
+		if err != nil {
+			log.Fatalf("failed listing all instances: %v", err)
+		}
+
+		if len(clusters) == 0 {
+			fmt.Println("There are no clusters currently under this account")
+			return
+		}
+
+		for i, cluster := range clusters {
+			fmt.Printf("(%v) %v\n", i+1, cluster.Name)
+		}
+		fmt.Printf("(b) go back\n\n")
+		selection := StringPrompt("Enter cluster index number to remove: ")
+		if selection == "b" {
+			return
+		}
+
+		// Try to delete selected cluster index number.
+		// index, err := strconv.Atoi(selection)
+		// if err != nil {
+		// 	log.Fatalf("invalid entry %v selection must be a number", selection)
+		// }
+
+		// err = client.DeleteLKECluster(context.Background(), clusters[index-1].ID)
+		// if err != nil {
+		// 	log.Fatalf("failed deleting instance: %v", err)
+		// }
+	}
 }
